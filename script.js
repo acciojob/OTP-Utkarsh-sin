ec0a5ce6a75cb181b785427aa5c23308af66c08d
Utkarsh-sin <56604999+Utkarsh-sin@users.noreply.github.com>
2023-05-28 12:39:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+function clickEvent(first,last){
+    if(first.value.length){
+      document.getElementById(last).focus();
+    }
+  }
+
+  function deleteEvent(last,first){
+    if(last.value.length==0){
+      document.getElementById(first).focus();
+    }
+  }
